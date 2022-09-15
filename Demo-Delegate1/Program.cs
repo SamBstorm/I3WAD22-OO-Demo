53594509b8a56cf7fe27d74b1021d260d20acf86
Samuel Legrain <samuel.legrain@bstorm.be>
2022-09-15 16:42:52 +0200

    Add Demonstrations Params + Delegate part1

---
+++ Demo-Delegate1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Demo_Delegate1
+{
+    delegate int OperationMath(int param1, int param2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            OperationMath ope = p.Division;
+            ope += p.Multiplication;
+            ope += p.Addition;
+            ope += p.Addition;
+            ope?.Invoke(5, 4); 
+            Console.WriteLine("Hello World!");
+        }
+
+        int Addition(int nb1, int nb2)
+        {
+            int result = nb1 + nb2;
+            Console.WriteLine(result);
+            return result;
+        }
+        int Multiplication(int nb1, int nb2)
+        {
+            int result = nb1 * nb2;
+            Console.WriteLine(result);
+            return result;
+        }
+        int Soustraction(int nb1, int nb2)
+        {
+            int result = nb1 - nb2;
+            Console.WriteLine(result);
+            return result;
+        }
+        int Division(int nb1, int nb2)
+        {
+            if (nb2 == 0) throw new ArgumentException();
+            int result = nb1 / nb2;
+            Console.WriteLine(result);
+            return result;
+        }
+    }
+}
---
+++ Demo-ParamStructVSParamRef/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Demo_ParamStructVSParamRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Coord2DStruct c1 = new Coord2DStruct { X = 1, Y = 1 };
+
+            Console.WriteLine($"X = {c1.X} | Y = {c1.Y}");
+            ChangeValueStruct(c1);
+            Console.WriteLine($"X = {c1.X} | Y = {c1.Y}");
+
+            Coord2DRef c2 = new Coord2DRef { X = 1, Y = 1 };
+
+            Console.WriteLine($"X = {c2.X} | Y = {c2.Y}");
+            ChangeValueRef(c2);
+            Console.WriteLine($"X = {c2.X} | Y = {c2.Y}");
+        }
+
+        static void ChangeValueStruct(Coord2DStruct st)
+        {
+            st.X = st.X * 2;
+            st.Y = st.Y * 2;
+        }
+
+        static void ChangeValueRef(Coord2DRef re)
+        {
+            re.X = re.X * 2;
+            re.Y = re.Y * 2;
+        }
+    }
+
+    public struct Coord2DStruct {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    public class Coord2DRef
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
